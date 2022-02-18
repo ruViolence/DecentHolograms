fcdfcf853313a548badc5f90fdfa935369834dc8
ruViolence <78062896+ruViolence@users.noreply.github.com>
2022-02-19 02:42:55 +0500

    Asynchronous ProtocolLib hook

--- src/main/java/eu/decentsoftware/holograms/api/nms/PacketHandler__ProtocolLib.java
+++ src/main/java/eu/decentsoftware/holograms/api/nms/PacketHandler__ProtocolLib.java
@@ -3,6 +3,7 @@ package eu.decentsoftware.holograms.api.nms;
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.ListenerOptions;
 import com.comphenix.protocol.events.ListenerPriority;
 import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketContainer;
@@ -10,11 +11,13 @@ import com.comphenix.protocol.events.PacketEvent;
 import eu.decentsoftware.holograms.api.DecentHologramsAPI;
 import org.bukkit.entity.Player;
 
+import java.util.Collections;
+
 public class PacketHandler__ProtocolLib {
 
     public PacketHandler__ProtocolLib() {
         ProtocolManager protocolManager = ProtocolLibrary.getProtocolManager();
-        protocolManager.addPacketListener(new PacketAdapter(DecentHologramsAPI.get().getPlugin(), ListenerPriority.NORMAL, PacketType.Play.Client.USE_ENTITY) {
+        protocolManager.addPacketListener(new PacketAdapter(DecentHologramsAPI.get().getPlugin(), ListenerPriority.NORMAL, Collections.singletonList(PacketType.Play.Client.USE_ENTITY), ListenerOptions.ASYNC) {
             @Override
             public void onPacketReceiving(PacketEvent event) {
                 if (event.getPacketType().equals(PacketType.Play.Client.USE_ENTITY)) {
